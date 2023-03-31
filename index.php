513e196e0d182b09b1cff7a70a540c678c4181e6
Giovannirighini2000 <righinigiovanni0@gmail.com>
2023-03-31 14:59:52 +0200

    feat added macro files structure ecommerce

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require_once './models/AnimaleCane.php';
+require_once './models/Cibo.php';
+require_once './models/Giocattolo.php';
+require_once './models/Cuccia.php';
+require_once './models/Animale.php';
+require_once './models/Cane.php';
+require_once './models/Gatto.php';
+require_once './models/Ciotola.php';
+?>
