774fb54c43258baa5c9afcf7cfcf4aad89749352
Giovannirighini2000 <righinigiovanni0@gmail.com>
2023-03-31 18:10:06 +0200

    eddit  clean code

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ require_once './models/Trasportino.php';
 
  $prodotto1Cibo = new Prodotto('COD-1fdsfd56', 'Bau Bau', '10 euro',new CategoriaAnimaleTipo ('cane'),new Cibo ('secco'));
 var_dump($prodotto1Cibo);
-$prodotto2 = new Prodotto('COD-12f5436', 'felix',' 30 euro',new CategoriaAnimaleTipo ('gatto'),new Cibo ('morbido'));
+$prodotto2Cibo = new Prodotto('COD-12f5436', 'felix',' 30 euro',new CategoriaAnimaleTipo ('gatto'),new Cibo ('morbido'));
 var_dump($prodotto2Cibo);
 
 
